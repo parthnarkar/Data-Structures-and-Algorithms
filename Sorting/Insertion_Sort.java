d8b7614622e0671740cb76048c12a5d896d785a2
Parth Narkar <parthnarkarofficial@gmail.com>
2024-12-11 01:29:09 +0530

    Create Insertion_Sort.java

---
+++ Sorting/Insertion_Sort.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void insertionSort(int arr[]) {
+        int n = arr.length;
+        
+        for(int i = 0 ; i<n ; i++){
+            
+            int j = i;
+            
+            while(j > 0 && arr[j-1] > arr[j]){
+                
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                
+                j--;
+            }
+        }
+    }
+}
